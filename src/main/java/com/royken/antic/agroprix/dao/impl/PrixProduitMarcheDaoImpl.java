2a3765b1053360e6a1029669d348b5d5bee5d36b
Kenfack Valmy-Roi <roykenvalmy@gmail.com>
2015-08-22 18:07:10 +0100

    Correction d'une petite erreur

--- src/main/java/com/royken/antic/agroprix/dao/impl/PrixProduitMarcheDaoImpl.java
+++ src/main/java/com/royken/antic/agroprix/dao/impl/PrixProduitMarcheDaoImpl.java
@@ -8,7 +8,6 @@ import com.royken.antic.agroprix.entities.Produit;
 import com.royken.antic.agroprix.entities.projection.PrixProduit;
 import com.royken.generic.dao.DataAccessException;
 import com.royken.generic.dao.impl.GenericDao;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
